795f36f36c8715b09aadd07cc822e2033eb49a92
Hasan-aga <careerforma@gmail.com>
2022-10-26 11:02:03 +0300

    use collected points for path

--- src/components/mapcontainer/path.component.tsx
+++ src/components/mapcontainer/path.component.tsx
@@ -1,43 +1,35 @@
 import { LatLngExpression, LatLngLiteral } from "leaflet";
 import { useEffect, useState } from "react";
 import { Polyline } from "react-leaflet";
-import { Route } from "../../contexts/routeTypes";
+import { Point } from "../../contexts/point.context";
 import { getRoute } from "../../utils/getRoute";
 
-// TODO: REMOVE ANY
-const Path = () => {
-  // TODO: draw a test line (polyline)
-
+const Path = ({ points }: { points: Point[] }) => {
   const [routeData, setRouteData] = useState<number[][][]>();
 
-  useEffect(() => {
-    const testPoints: LatLngLiteral[] = [
-      { lat: 52.37916210613177, lng: 4.859168033949572 },
-      {
-        lat: 52.361659073117764,
-        lng: 4.880517577487824,
-      },
-    ];
-    const drawRoute = async () => {
-      const results = await getRoute(testPoints);
-      // console.log(
-      //   "results: ",
-      //   results.features[0].geometry.coordinates[0].map((latLng) => [
-      //     latLng[1],
-      //     latLng[0],
-      //   ])
-      // );
+  const getStartAndEnd = (points: Point[]): LatLngLiteral[] | undefined => {
+    const startPoint = points.find((p) => p.type === "start");
+    const endPoint = points.find((p) => p.type === "end");
+    if (!startPoint || !endPoint) return;
+    return [startPoint.coords, endPoint.coords];
+  };
 
+  useEffect(() => {
+    const startAndEndPoints = getStartAndEnd(points);
+    const drawRoute = async () => {
+      const results = startAndEndPoints && (await getRoute(startAndEndPoints));
+      if (!results) return;
       setRouteData(results.features[0].geometry.coordinates);
     };
     drawRoute();
-  }, []);
+  }, [points]);
   return (
     <>
       {routeData &&
         routeData.map((path) => {
           return (
             <Polyline
+              key={path[0][0]}
               pathOptions={{ color: "red" }}
               positions={
                 path.map((latLng) => [
@@ -48,18 +40,6 @@ const Path = () => {
             />
           );
         })}
-      {/* {routeData && (
-
-        <Polyline
-          pathOptions={{ color: "red" }}
-          positions={
-            routeData.map((latLng) => [
-              latLng[1],
-              latLng[0],
-            ]) as LatLngExpression[]
-          }
-        />
-      )} */}
     </>
   );
 };
