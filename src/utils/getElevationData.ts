9a0288f7c00fe3aa7cfc24550b8951a8ca5d2186
Hasan-aga <careerforma@gmail.com>
2022-10-27 11:29:44 +0300

    make ready the elevation data for chartjs

---
+++ src/utils/getElevationData.ts
@@ -0,0 +1,54 @@
+import { Route } from "./routeTypes";
+
+export function calculateElevationProfileData(routeData: Route) {
+  const legElevations: number[][][] = [];
+
+  // elevation_range contains pairs [distance, elevation] for every leg geometry point
+  routeData.features[0].properties.legs.forEach((leg) => {
+    if (leg.elevation_range) {
+      legElevations.push(leg.elevation_range);
+    } else {
+      legElevations.push([]);
+    }
+  });
+  const labels: number[] = [];
+  const data: number[] = [];
+
+  legElevations.forEach((legElevation, index) => {
+    let previousLegsDistance = 0;
+    for (let i = 0; i <= index - 1; i++) {
+      previousLegsDistance += legElevations[i][legElevations[i].length - 1][0];
+    }
+
+    labels.push(
+      ...legElevation.map(
+        (elevationData) => elevationData[0] + previousLegsDistance
+      )
+    );
+    data.push(...legElevation.map((elevationData) => elevationData[1]));
+  });
+
+  // optimize array size to avoid performance problems
+  const labelsOptimized: number[] = [];
+  const dataOptimized: number[] = [];
+  const minDist = 5; // ~5m
+  const minHeight = 10; // ~10m
+
+  labels.forEach((dist, index) => {
+    if (
+      index === 0 ||
+      index === labels.length - 1 ||
+      dist - labelsOptimized[labelsOptimized.length - 1] > minDist ||
+      Math.abs(data[index] - dataOptimized[dataOptimized.length - 1]) >
+        minHeight
+    ) {
+      labelsOptimized.push(dist);
+      dataOptimized.push(data[index]);
+    }
+  });
+
+  return {
+    data: dataOptimized,
+    labels: labelsOptimized,
+  };
+}
