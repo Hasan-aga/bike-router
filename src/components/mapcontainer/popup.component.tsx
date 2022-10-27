22330afd2f2ca5cd4be8fa477792d12578aff222
Hasan-aga <careerforma@gmail.com>
2022-10-27 21:33:29 +0300

    make complex menu popup logic

--- src/components/mapcontainer/popup.component.tsx
+++ src/components/mapcontainer/popup.component.tsx
@@ -1,15 +1,15 @@
 import { LatLngLiteral, Map, Popup as LeafletPopup } from "leaflet";
 import { useCallback, useContext, useEffect, useRef } from "react";
 import { Popup } from "react-leaflet";
-import { Point, pointContext } from "../../contexts/point.context";
+import { Point, pointContext, PointType } from "../../contexts/point.context";
 import PopupButton from "../popupButton/popupButton.component";
 import "./popup.style.scss";
 
-type Props = {
+interface Props {
   map: Map;
   point: Point;
   setTemporaryPoint: React.Dispatch<React.SetStateAction<Point | undefined>>;
-};
+}
 
 const startPointExists = (points: Point[]): boolean =>
   points.find((p) => p.type === "start") !== undefined;
@@ -28,45 +28,121 @@ const Popmenu = ({ map, point, setTemporaryPoint }: Props) => {
     [map]
   );
 
+  type ExistingPoints =
+    | "startExists"
+    | "endExists"
+    | "startAndEndExist"
+    | "noneExists";
+
+  const menuSelector: Record<ExistingPoints, JSX.Element> = {
+    noneExists: (
+      <div className="popup">
+        <PopupButton
+          point={point}
+          type="start"
+          setTemporaryPoint={setTemporaryPoint}
+        />
+        <PopupButton
+          point={point}
+          type="end"
+          setTemporaryPoint={setTemporaryPoint}
+        />
+      </div>
+    ),
+    startExists: (
+      <div className="popup">
+        <PopupButton
+          point={point}
+          type="end"
+          setTemporaryPoint={setTemporaryPoint}
+        />
+      </div>
+    ),
+    endExists: (
+      <div className="popup">
+        <PopupButton
+          point={point}
+          type="start"
+          setTemporaryPoint={setTemporaryPoint}
+        />
+      </div>
+    ),
+    startAndEndExist: (
+      <div className="popup">
+        <PopupButton
+          point={point}
+          type="mid"
+          setTemporaryPoint={setTemporaryPoint}
+        />
+      </div>
+    ),
+  };
+
   useEffect(() => {
     displayPopup(point.coords);
   }, [point, displayPopup]);
 
-  switch (point.type) {
-    case "temporary":
-      return (
-        <Popup ref={popupRef}>
-          <div className="popup">
-            {!startPointExists(points) && (
-              <PopupButton
-                point={point}
-                type="start"
-                setTemporaryPoint={setTemporaryPoint}
-              />
-            )}
+  const getExistingPoints = (points: Point[]): ExistingPoints => {
+    const pointTypeSet = new Set<PointType>();
+    points.forEach((p) => pointTypeSet.add(p.type));
+    if (pointTypeSet.has("start") && pointTypeSet.has("end"))
+      return "startAndEndExist";
+    if (pointTypeSet.has("start")) return "startExists";
+    if (pointTypeSet.has("end")) return "endExists";
+    return "noneExists";
+  };
 
-            <PopupButton
-              point={point}
-              type="end"
-              setTemporaryPoint={setTemporaryPoint}
-            />
-          </div>
-        </Popup>
-      );
+  return (
+    <Popup ref={popupRef}>
+      {point.type === "temporary" ? (
+        menuSelector[getExistingPoints(points)]
+      ) : (
+        <div className="popup">
+          <PopupButton
+            point={point}
+            type="remove"
+            setTemporaryPoint={setTemporaryPoint}
+          />
+        </div>
+      )}
+    </Popup>
+  );
 
-    default:
-      return (
-        <Popup ref={popupRef}>
-          <div className="popup">
-            <PopupButton
-              point={point}
-              type="remove"
-              setTemporaryPoint={setTemporaryPoint}
-            />
-          </div>
-        </Popup>
-      );
-  }
+  // switch (point.type) {
+  //   case "temporary":
+  //     return (
+  //       <Popup ref={popupRef}>
+  //         <div className="popup">
+  //           {!startPointExists(points) && (
+  //             <PopupButton
+  //               point={point}
+  //               type="start"
+  //               setTemporaryPoint={setTemporaryPoint}
+  //             />
+  //           )}
+
+  //           <PopupButton
+  //             point={point}
+  //             type="end"
+  //             setTemporaryPoint={setTemporaryPoint}
+  //           />
+  //         </div>
+  //       </Popup>
+  //     );
+
+  //   default:
+  //     return (
+  //       <Popup ref={popupRef}>
+  //         <div className="popup">
+  //           <PopupButton
+  //             point={point}
+  //             type="remove"
+  //             setTemporaryPoint={setTemporaryPoint}
+  //           />
+  //         </div>
+  //       </Popup>
+  //     );
+  // }
 };
 
 export default Popmenu;
