3f76863134a6e3f2f6d590cd41985e7e63a974e4
Hasan-aga <careerforma@gmail.com>
2022-10-29 22:48:18 +0300

    use area chart

--- src/components/elevationChart/elevationChart.component.tsx
+++ src/components/elevationChart/elevationChart.component.tsx
@@ -1,7 +1,7 @@
 import { Route } from "../../utils/routeTypes";
 import { calculateElevationProfileData } from "../../utils/getElevationData";
 import "./elevationChart.style.scss";
-import { LineChart, Line, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { ResponsiveContainer, XAxis, YAxis, AreaChart, Area } from "recharts";
 import { Tooltip } from "react-leaflet";
 
 const ElevationChart = ({ pathData }: { pathData: Route }) => {
@@ -10,12 +10,27 @@ const ElevationChart = ({ pathData }: { pathData: Route }) => {
   return (
     <div className="elevation-chart">
       <ResponsiveContainer width="100%" height={340}>
-        <LineChart data={chartData}>
-          <Tooltip />
-          <Line type="monotone" dataKey="data" stroke="#8884d8" />
+        <AreaChart
+          data={chartData}
+          onMouseMove={(e) => console.log(e?.activeLabel)}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
           <XAxis dataKey="label" />
           <YAxis dataKey="data" />
-        </LineChart>
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="data"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+        </AreaChart>
       </ResponsiveContainer>
     </div>
   );
