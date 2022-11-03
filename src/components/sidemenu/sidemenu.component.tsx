f7082780ac818c6da2efc80817c7d9221b3d3285
Hasan-aga <careerforma@gmail.com>
2022-11-03 12:35:16 +0300

    add avg inclination %

--- src/components/sidemenu/sidemenu.component.tsx
+++ src/components/sidemenu/sidemenu.component.tsx
@@ -6,12 +6,11 @@ import Search from "../search/search.component";
 import "./sidemenu.style.scss";
 const Sidemenu = () => {
   const { inclination } = useContext(inclinationContext);
-  console.log("inclination:", inclination);
-
+  const info = inclination ? <p> Average inclination: {inclination}%</p> : "";
   return (
     <div className="sidemenu scroll scroll-5">
       <Search placeholder="Search location" />
-      {inclination && <p> Average inclination: {inclination}%</p>}
+      {info}
     </div>
   );
 };
