36db9bcc68fbe666f8ff4f431f3cd88cd005b0a4
Hasan-aga <careerforma@gmail.com>
2022-11-04 20:09:49 +0300

    use befireeach e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,17 +1,16 @@
-import { createYield } from "typescript";
-
 describe("testing map", () => {
-  it("can search for location", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000/");
+  });
+
+  it("can search for location", () => {
     cy.findByPlaceholderText(/search location/i).type("mersin\n");
   });
 
   it("can add marker to map", () => {
-    cy.visit("http://localhost:3000/");
     cy.get("#root > div > div:nth-child(1)").click();
   });
   it("can create a path on map", () => {
-    cy.visit("http://localhost:3000/");
     const map = cy.get("#root > div > div:nth-child(1)");
     map.click();
     cy.findByRole("button", { name: /set as start/i }).click();
@@ -19,7 +18,6 @@ describe("testing map", () => {
     cy.findByRole("button", { name: /set as end/i }).click();
   });
   it("elevation chart and slope are displayed", () => {
-    cy.visit("http://localhost:3000/");
     const map = cy.get("#root > div > div:nth-child(1)");
     map.click();
     cy.findByRole("button", { name: /set as start/i }).click();
