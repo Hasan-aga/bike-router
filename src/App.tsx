92e5ac35bde27e097e03c7cc35120cf0d1cac880
Hasan-aga <careerforma@gmail.com>
2022-11-15 23:02:41 +0300

    move dashboard to true bottom of screen solve 100vh mobile problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.scss";
 import Dashboard from "./components/dashboard/dashboard.component";
 import Mapcontainer from "./components/mapcontainer/mapcontainer.component";
@@ -9,6 +10,19 @@ import { PointProvider } from "./contexts/point.context";
 import { SearchProvider } from "./contexts/search.context";
 
 function App() {
+  function setPageHeight() {
+    const { innerHeight } = window;
+    let root = document.documentElement;
+    const heightPx = `${innerHeight}px`;
+    console.log(heightPx);
+
+    root.style.setProperty("--doc-height", heightPx);
+  }
+
+  useEffect(() => {
+    setPageHeight();
+  }, []);
+
   return (
     <div className="App">
       <SearchProvider>
