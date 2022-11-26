f4256a4c0ac3cfc88e5cf4179281b99021abbf19
Hasan-aga <careerforma@gmail.com>
2022-11-26 22:39:30 +0300

    use generic serverless function for waypoints

--- src/utils/getRoute.ts
+++ src/utils/getRoute.ts
@@ -8,9 +8,13 @@ export const getRoute = async (points: LatLngLiteral[]) => {
     })
     .join("|");
 
-  const url = `https://us-central1-neat-episode-365710.cloudfunctions.net/attachAPIkey?url=https://api.geoapify.com/v1/routing?waypoints=${waypoints}&mode=bicycle&details=elevation`;
+  const url = `https://us-central1-neat-episode-365710.cloudfunctions.net/attachAPIkey`;
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      body: JSON.stringify({
+        url: `https://api.geoapify.com/v1/routing?waypoints=${waypoints}&mode=bicycle&details=elevation`,
+      }),
+    });
 
     const result: Route = await response.json();
     console.log(result);
