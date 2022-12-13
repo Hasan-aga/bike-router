093359f099d48d38d06ee4a8c6e072be103eb3dd
Hasan-aga <careerforma@gmail.com>
2022-12-13 13:52:32 +0300

    add markdown parsing

--- src/components/about/policy.component.tsx
+++ src/components/about/policy.component.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 import "./policy.style.scss";
-
+import policy from "./policy.md";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 export default function Policy() {
+  const [post, setPost] = useState<string>();
+  useEffect(() => {
+    fetch(policy)
+      .then((res) => res.text())
+      .then((res) => setPost(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="policy">
-      <h1>Policy</h1>
+      {post && <ReactMarkdown>{post}</ReactMarkdown>}
     </div>
   );
 }
